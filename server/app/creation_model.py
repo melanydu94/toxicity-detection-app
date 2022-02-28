bf9d601bbd8ee790642691ef3ad8c9da7193b92f
DanySone <dany.sonethavy@efrei.net>
2022-02-28 06:23:06 -0500

    add toxicity detection model to the project

--- server/app/creation_model.py
+++ server/app/creation_model.py
@@ -1,36 +1,22 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from detoxify import Detoxify
+from detoxify import Detoxify    
+import pandas as pd
 
-def response_dict(sentiment, pos_score, neu_score, neg_score):
+
+
+def response_dict(toxic_score, sev_toxic_score, obscene_score, threat_score, insult_score, id_hate_score):
     my_dict = {
-        "Sentiment": sentiment,
-        "Rating_Positive": pos_score,
-        "Rating_Neutral": neu_score,
-        "Rating_Negative": neg_score,
+        "toxic": toxic_score,
+        "severe_toxic": sev_toxic_score,
+        "obscene": obscene_score,
+        "threat": threat_score,
+        "insult": insult_score,
+        "identity_hate": id_hate_score
     }
     return my_dict
 
 def sentiment_scores(sentence):
-    sid_obj = SentimentIntensityAnalyzer()
-
-    # polarity_scores method of SentimentIntensityAnalyzer
-    # object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
-    sentiment_dict = sid_obj.polarity_scores(sentence)
-
-    print("Overall sentiment dictionary is : ", sentiment_dict)
-    print("sentence was rated as ", sentiment_dict['neg'] * 100, "% Negative")
-    print("sentence was rated as ", sentiment_dict['neu'] * 100, "% Neutral")
-    print("sentence was rated as ", sentiment_dict['pos'] * 100, "% Positive")
-
-    # decide sentiment as positive, negative and neutral
-    if sentiment_dict['compound'] >= 0.05:
-        sent = "Positive"
-    elif sentiment_dict['compound'] <= - 0.05:
-        sent = "Negative"
-    else:
-        sent = "Neutral"
-    return response_dict(sent, round(sentiment_dict['pos'], 2), round(sentiment_dict['neu'], 2), round(sentiment_dict['neg'], 2))
+    results = Detoxify('original').predict(sentence)
+    # print(pd.DataFrame(results, index=sentence).round(5).to_json(orient="index"))
+    return pd.DataFrame(results, index=sentence).round(5).to_json(orient="index")
     
-
-
+sentiment_scores(["I HATE YOU!!!!!!!", "I LOVE YOU !!!!"])
