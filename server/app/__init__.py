be9305eda57dbebcf5d7a4d97e81977e4188b61a
DanySone <dany.sonethavy@efrei.net>
2022-03-04 08:21:42 +0100

    add alert (in progress)

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 from flask_cors import CORS, cross_origin
 from . import creation_model
 import json
+import prometheus_client
+from prometheus_client.core import CollectorRegistry
+from prometheus_client import Summary, Counter, Histogram, Gauge
+import time
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +16,11 @@ app.testing = True
 HOST = "127.0.0.1"
 PORT = 5000
 
+_INF = float('inf')
+
+graphs = {}
+graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
+graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds', buckets={1, 2, 5, 6, 10, _INF})
 
 @app.route('/')
 def index():
@@ -18,12 +29,25 @@ def index():
 
 @app.route('/model', methods=['POST'])
 def model():
+    start = time.time()
+    graphs['c'].inc()
+
     print("Enter a sentence to analyse :")
+
     sentence = request.json['sentence']
     result = creation_model.sentiment_scores([sentence])
     json_result = json.dumps(result)
+    time.sleep(0.600)
+    end = time.time()
+    graphs['h'].observe(end - start)
     return json_result, 200
 
+@app.route('/metrics', methods=['GET'])
+def requests_count():
+    res = []
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+    return Response(res, mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(HOST, debug=True)
--- server/app/creation_model.py
+++ server/app/creation_model.py
@@ -1,13 +1,10 @@
 from detoxify import Detoxify    
 import pandas as pd
-from torch import set_rng_state
 
 
 def sentiment_scores(sentence):
     results = Detoxify(checkpoint="app/toxic_original-c1212f89.ckpt", device="cpu").predict(sentence)
-    # print(pd.DataFrame(results, index=sentence).round(5).to_json(orient="index"))
     df = pd.DataFrame(results, index=sentence).round(5)
     my_dict = df.rename_axis('sentence').reset_index().to_dict('index')
     response_dict = list(my_dict.values())[0]
     return response_dict 
-# print(sentiment_scores(["I HATE YOU!!!!!!!", "I LOVE YOU !!!!"]))
