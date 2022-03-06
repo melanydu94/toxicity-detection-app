0a0e09f3c74f4c2a070eb66afbf3273668df94cd
DanySone <dany.sonethavy@efrei.net>
2022-03-06 13:17:28 +0100

    fix bugs app

--- server/app/creation_model.py
+++ server/app/creation_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def sentiment_scores(sentence):
-    results = Detoxify(checkpoint="server/app/toxic_original-c1212f89.ckpt", device="cpu").predict(sentence)
+    results = Detoxify(checkpoint="app/toxic_original-c1212f89.ckpt", device="cpu").predict(sentence)
     df = pd.DataFrame(results, index=sentence).round(5)
     my_dict = df.rename_axis('sentence').reset_index().to_dict('index')
     response_dict = list(my_dict.values())[0]
